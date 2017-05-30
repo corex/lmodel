b7ffd2b175a8f942369db830acf7cf41e2b94941
CoRex <dev@corex.dk>
2017-05-30 21:04:30 +0200

    Added code to check for database connection when testing.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,4 +1,12 @@
 <?php
+
+use Illuminate\Config\Repository;
+use Illuminate\Container\Container;
+use Illuminate\Database\Connectors\ConnectionFactory;
+use Illuminate\Database\DatabaseManager;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Facade;
+
 require_once(dirname(__DIR__) . '/vendor/autoload.php');
 
 try {
@@ -12,6 +20,21 @@ try {
         ];
         throw new Exception("\n" . implode("\n", $message) . "\n");
     }
+
+    // Test connection.
+    $app = new Container();
+    $app->singleton('app', Container::class);
+    $app->singleton('config', Repository::class);
+    $config = require(dirname(__DIR__) . '/database.php');
+    $connection = $config['default'];
+    $app['config']->set('database.default', $connection);
+    $app['config']->set('database.connections.' . $connection, $config['connections'][$connection]);
+    $app->bind('db', function ($app) {
+        return new DatabaseManager($app, new ConnectionFactory($app));
+    });
+    Facade::setFacadeApplication($app);
+    DB::connection($connection)->getPdo();
+
 } catch (Exception $e) {
     print($e->getMessage() . "\n");
     exit;
